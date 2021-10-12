1cae9143c955f75ab9c7626b0d816aedc20667df
CatherineJang <catherinehjang@gmail.com>
2021-10-12 03:51:38 -0700

    more incorrect changes

--- src/Task.js
+++ src/Task.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Task(props) {
     console.log(props.taskText);
     return <div>
-        <div className="taskText"><input type="checkbox">{props.taskText}</input></div>
+        <div className="taskText"><input type="checkbox">{props.title}</input></div>
         <div className="editButton"><button>Edit</button></div>
         <div className="deleteButton"><button>Delete</button></div>
     </div>
