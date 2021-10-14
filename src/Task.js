3e5ddc3c16cb472ae28dd1c6232576f2f5340c48
CatherineJang <catherinehjang@gmail.com>
2021-10-13 22:52:07 -0700

    things are editable

--- src/Task.js
+++ src/Task.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Task(props) {
     console.log("in Task");
     return <div key={props.id}>
-        <div className="taskText"><input type="checkbox"/>{props.taskText}</div>
+        <div className="taskText">
+            <input type="checkbox"/>
+            <p contentEditable="true">{props.taskText}</p>
+        </div>
     </div>
 }
 
--- src/Tasks.js
+++ src/Tasks.js
@@ -4,13 +4,18 @@ import React, {useState} from "react";
 function Tasks(props) {
     const [completedId, setCompletedId] = useState([]);
     console.log("in Tasks");
+
+    function toggleEdit(){
+
+    }
+
     return (<div>
         {props.list.map(elem => <div>
                                     <Task onRowClick={(id) => setCompletedId(completedId.append(id))}
                                       completed={elem.id in completedId}
                                       key={elem.id}
                                       taskText={elem.title} {...elem}/>
-                                    <div className="editButton" ><button>Edit</button></div>
+                                    {/*<div className="editButton"><button onClick={e => toggleEdit()}>Edit</button></div>*/}
                                     <div className="deleteButton"><button onClick={e => props.onDeleteTask(elem.id)}>Delete</button></div>
                                 </div>)}
     </div>);
