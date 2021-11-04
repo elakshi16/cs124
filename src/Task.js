1cd4744f1a8d7e1bb042715198dbe9841172e5b2
Elakshi Shah <eshah@hmc.edu>
2021-11-03 23:45:43 -0700

    priority firestore setup

--- src/Task.js
+++ src/Task.js
@@ -7,6 +7,11 @@ function Task(props) {
         <div id="task">
             <input type="checkbox" className="check" checked={props.completed} onChange={e => props.onTaskFieldChange(props.id, "completed",e.target.checked)}/>
             <input type="text" value={props.text} className={'writing'} onChange={e => props.onTaskFieldChange(props.id, "title",e.target.value)}/>
+            <div id={"priority"}>
+                <button className={"priorityButton"} onClick={e => props.onTaskFieldChange(props.id, "priority", 1)}/>
+                <button className={"priorityButton"} onClick={e => props.onTaskFieldChange(props.id, "priority", 2)}/>
+                <button className={"priorityButton"} onClick={e => props.onTaskFieldChange(props.id, "priority", 3)}/>
+            </div>
             <button className={"deleteButton"} onClick={e => props.onDeleteTask([props.id])}>Delete</button>
         </div>
 
